5790a33b2aa0843c1d8a14987bb5f64630c56d32
Namuli-Cissy <cissynamuli5@gmail.com>
2024-12-05 14:09:04 +0300

    Add files via upload

---
+++ NAMULI CISSY FINAL/cissy.js
@@ -0,0 +1,14 @@
+function Functions(){
+	alert("Good Evening");
+	alert("Good Evening to you too");
+	alert("How are you?");
+	alert("Am okay");
+	alert("What is your favorite color?");
+	alert("It's green");
+}
+function Events(){
+	alert("AN ANNOINTMENT PRAYER DAY!!!!!");
+	alert("IT WILL GONNA BE A HEALING DAY OF ALL KINDS OF DISEASES");
+	alert("COME AND WITNESS THE POWER OF A PRAYER ON THAT VERY DAY.");
+	alert("THE GUEST OF HONOR WILL BE GOD!!!!!!!!");
+}
